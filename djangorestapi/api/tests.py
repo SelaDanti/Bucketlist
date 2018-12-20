dddb52bcf9b6f444bc2dec5691b282fcbec752c2
George Mutti <georgemutti@Georges-MacBook-Pro.local>
2018-12-20 14:22:15 +0300

    [Chore #162754321]Test models

--- djangorestapi/api/tests.py
+++ djangorestapi/api/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import ToDoList
 
-# Create your tests here.
+class ModleTestList(TestCase):
+    """Test the bucket list Model."""
+
+    def setUp(self):
+        """Defining the test client."""
+        self.item_name = "This is my first project"
+        self.todo_list = ToDoList(name=self.item_name)
+
+    def test_model_can_create_a_todo_list(self):
+        """Test the bucketlist model can create a bucketlist."""
+        initial_count = ToDoList.objects.count()
+        self.todo_list.save()
+        new_count = ToDoList.objects.count()
+        self.assertNotEqual(initial_count, new_count)
