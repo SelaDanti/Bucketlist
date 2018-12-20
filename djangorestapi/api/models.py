298d204180c748bb2113164ec1893ceb944b0d99
George Mutti <georgemutti@Georges-MacBook-Pro.local>
2018-12-20 14:55:25 +0300

    [Chore #162754508]Add table models

--- djangorestapi/api/models.py
+++ djangorestapi/api/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+#api/models
 
-# Create your models here.
+from django.db  import models
+
+class ToDoList(models.Model):
+    """This is a representation of buckectlist model."""
+    item_name = models.CharField(max_length=200, blank=False, unique=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    complete=models.BooleanField(default=False)
+
+    def __str__(self):
+        """Readable Output for users."""
+        return "{}".format(self.item_name)
