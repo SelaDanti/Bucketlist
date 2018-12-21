aa3567e5f2dbd8d38d7356c61bee0b2338b6211a
MaggieChege <shikooh.maggie@gmail.com>
2018-12-21 12:40:28 +0300

    Revert "Ch update readme"

--- djangorestapi/authapi/urls.py
+++
@@ -1,8 +0,0 @@
-"""authapi/urls.py"""
-from django.urls import path , include
-
-urlpatterns = [
-    path('users/', include('api.urls')),
-    # login route http://127.0.0.1:8000/api/rest-auth/login/
-    path('rest-auth/', include('rest_auth.urls')),
-]
